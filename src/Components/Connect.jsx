1c4a3d48a43ae349f958a3e9dcf2452b8bd53513
kallolpandit <kallolpandit466@gmail.com>
2023-08-04 04:27:37 +0600

    connect text

--- src/Components/Connect.jsx
+++ src/Components/Connect.jsx
@@ -12,6 +12,24 @@ export const Connect = () => {
             Connect people in digital life
           </h2>
         </div>
+        <div className="flex">
+          <div className="w-2/5">
+            <img src="images/connect-people.jpg" alt="connect-people" />
+          </div>
+          <div className="w-3/5 bg-orange-100">
+            <h3>
+              More than just an ad agency, we harness the tools of traditional
+              and digital.
+            </h3>
+            <p>
+              With over 25 years of experience, we have crafted thousands of
+              strategic discovery process that enables us to peel back the
+              layers which enable us to understand, connect, represent and
+              dominate your market.
+            </p>
+            <a href="">More about us</a>
+          </div>
+        </div>
       </div>
     </section>
   );

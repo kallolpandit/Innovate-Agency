ce8882c3cf84a6591af7aacc71a50a37a0458e29
kallolpandit <kallolpandit466@gmail.com>
2023-08-04 15:35:01 +0600

    connect

--- src/Components/Connect.jsx
+++ src/Components/Connect.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Connect = () => {
   return (
-    <section>
+    <section className="relative">
       <div className="container">
         <div>
           <h4 className="text-xl text-brand font-poppins font-medium capitalize pb-3">
@@ -13,21 +13,26 @@ export const Connect = () => {
           </h2>
         </div>
         <div className="flex">
-          <div className="w-2/5">
+          <div className="w-[45%] absolute inset-1/3 left-0">
             <img src="images/connect-people.jpg" alt="connect-people" />
           </div>
-          <div className="w-3/5 bg-orange-100">
-            <h3>
+          <div className="w-3/5 bg-orange-100 pl-28 ml-auto">
+            <h3 className="text-primary text-4xl font-poppins font-semibold w-full lg:w-[570px] capitalize leading-tight pt-20">
               More than just an ad agency, we harness the tools of traditional
               and digital.
             </h3>
-            <p>
+            <p className="text-secondary text-lg font-nunito font-normal w-full lg:w-[500px] capitalize leading-normal pt-5 pb-12">
               With over 25 years of experience, we have crafted thousands of
               strategic discovery process that enables us to peel back the
               layers which enable us to understand, connect, represent and
               dominate your market.
             </p>
-            <a href="">More about us</a>
+            <a
+              href=""
+              className="text-white text-lg font-nunito font-semibold capitalize py-3 px-7 rounded-2xl bg-brand inline-block mb-10 border hover:bg-transparent hover:text-brand hover:border-brand transition-all"
+            >
+              More about us
+            </a>
           </div>
         </div>
       </div>
